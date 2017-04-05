ac5cfad1f5755808a2f92cd76ade3d1306aa3f80
rdtHK <mariop.36754@gmail.com>
2017-04-05 20:06:34 -0300

    Basic implementation of FilesystemCache.

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 
 - [ ] Maybe change render to return a string instead of printing directly?
     - [ ] Maybe both? render & print methods
-- [ ] Cache
-    - [ ] InMemoryCache
+- [x] Cache
+    - [x] InMemoryCache
     - [ ] FilesystemCache
+        - [ ] Check permissions
+        - [ ] Throw exceptions
 - [ ] Do something about %> inside strings
 - [x] A way of extending templates
     Using $GLOBALS seems kinda ugly but I'm tired and lazy.
---
+++ src/Cache/FilesystemCache.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2017 Mário Camargo Palmeira
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Rdthk\EmbeddedPHP\Cache;
+
+class FilesystemCache implements Cache
+{
+    private $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function exists(string $template): bool
+    {
+        $path = $this->path . DIRECTORY_SEPARATOR . $template;
+        return file_exists($path);
+    }
+
+    public function load(string $template): ?string
+    {
+        $path = $this->path . DIRECTORY_SEPARATOR . $template;
+        return file_get_contents($path);
+    }
+
+    public function store(string $template, string $code): void
+    {
+        $path = $this->path . DIRECTORY_SEPARATOR . $template;
+        file_put_contents($path, $code);
+    }
+}
